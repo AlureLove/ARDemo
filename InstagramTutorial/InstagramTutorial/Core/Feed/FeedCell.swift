ce86c07f03ff36bd1726848020d93494d62f67ab
AlureLove <qhlrx@qq.com>
2024-09-30 16:26:01 +0900

    feat: feed view
    add feed view

---
+++ InstagramTutorial/InstagramTutorial/Core/Feed/FeedCell.swift
@@ -0,0 +1,98 @@
+//
+//  FeedCell.swift
+//  InstagramTutorial
+//
+//  Created by JethroLiu on 2024/9/30.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        VStack {
+            // image + username
+            HStack {
+                Image("batman-2")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                Text("batman")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            
+            // post image
+            
+            Image("batman-2")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .clipShape(Rectangle())
+            
+            // action buttons
+            HStack(spacing: 16) {
+                Button {
+                    print("Like Post")
+                } label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                }
+
+                Button {
+                    print("Comment on post")
+                } label: {
+                    Image(systemName: "bubble.right")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    print("Share post")
+                } label: {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            .padding(.top, 4)
+            .foregroundStyle(.black)
+            
+            // likes label
+            
+            Text("23 Likes")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+            
+            // caption label
+            HStack {
+                Text("batman").fontWeight(.semibold) +
+                Text("This is some test caption for now this is some test caption")
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .font(.footnote)
+            .padding(.leading, 10)
+            .padding(.top, 1)
+            
+            Text("6 hours ago")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+                .foregroundStyle(.gray)
+        }
+    }
+}
+
+#Preview {
+    FeedCell()
+}
---
+++ InstagramTutorial/InstagramTutorial/Core/Feed/FeedView.swift
@@ -0,0 +1,41 @@
+//
+//  FeedView.swift
+//  InstagramTutorial
+//
+//  Created by JethroLiu on 2024/9/30.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack(spacing: 32) {
+                    ForEach(0...10, id: \.self) { post in
+                        FeedCell()
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("instagram-black")
+                        .resizable()
+                        .frame(width: 100, height: 32)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    FeedView()
+}
